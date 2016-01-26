4e711f558ff9b5664fd38b270b1502566625fa30
Filip Krstevski <fkrstevski@gmail.com>
2016-01-26 13:29:12 -0500

    Initial project

---
+++ Assets/Scripts/GoalScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoalScript : MonoBehaviour {
+
+	void OnTriggerEnter (Collider collider) {
+		GameObject collidedWith = collider.gameObject;
+
+		if (collidedWith.tag == gameObject.tag) {
+			GetComponent<Light>().intensity = 0;
+			Destroy (collidedWith);
+		}
+	}
+}
